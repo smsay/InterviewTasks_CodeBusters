779be2284ea08ee2e67763ae93d6930d738b8e89
Sumeyse <sumeyse@gmail.com>
2022-08-20 11:04:51 +0300

    Pls do not create e new class, you can write your solution in the same question class with a method with your name

--- src/SortLettersAndNumbers.java
+++ src/SortLettersAndNumbers.java
@@ -1,4 +1,44 @@
 /*Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.*/
 
 public class SortLettersAndNumbers {
+
+    public static void main(String[] args) {
+        String given = "CDB098dbc90ba";
+        System.out.println(given);
+        System.out.println(sortPartsByOl_Me(given));
+    }
+
+    private static String sortPartsByOl_Me(String given) {
+
+        ArrayList<String> parts = new ArrayList<>();
+
+        while (given.length() > 0) {
+            boolean isLetter = Character.isLetter(given.charAt(0));
+            String result = "";
+            if (isLetter) {
+                for (char ch : given.toCharArray()) {
+                    if (!Character.isLetter(ch)) break;
+                    result += "" + ch;
+                }
+                parts.add(result);
+            } else {
+                for (char ch : given.toCharArray()) {
+                    if (Character.isLetter(ch)) break;
+                    result += "" + ch;
+                }
+                parts.add(result);
+            }
+            given = given.substring(result.length());
+        }
+
+        for (String eachPart : parts) {
+            char[] arr = eachPart.toCharArray();
+            Arrays.sort(arr);
+            for (char ch : arr) {
+                given += ch;
+            }
+        }
+
+        return given;
+    }
 }
