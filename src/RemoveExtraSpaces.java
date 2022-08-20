e3b0b496ae414c30a7c41054276256bedc8b72d9
Sumeyse <sumeyse@gmail.com>
2022-08-20 11:06:28 +0300

    Pls do not create e new class, you can write your solution in the same question class with a method with your name

---
+++ src/RemoveExtraSpaces.java
@@ -0,0 +1,13 @@
+/*
+        Remove All Extra Space from String
+        EXAMPLE:
+        Input: "  Hello world      I      love      Java    "
+        Output: Hello world I love Java
+     */
+
+
+
+public class RemoveExtraSpaces {
+
+
+}
